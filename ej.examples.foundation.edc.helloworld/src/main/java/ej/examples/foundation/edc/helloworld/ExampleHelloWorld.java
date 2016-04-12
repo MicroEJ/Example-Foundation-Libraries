9778e57d6c4ceee411a8e3cb8e360baa46ff3a02
privron <privron@is2t-intern.com>
2016-04-12 12:14:12 +0200

    add ej.examples.foundation.edc.helloworld

---
+++ ej.examples.foundation.edc.helloworld/src/main/java/ej/examples/foundation/edc/helloworld/ExampleHelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Java
+ * 
+ * Copyright 2011-2012 IS2T. All rights reserved.
+ * For demonstration purpose only.
+ * IS2T PROPRIETARY. Use is subject to license terms.
+ */
+package ej.examples.foundation.edc.helloworld;
+
+
+/**
+ * Prints the message "Hello World !"
+ */
+public class ExampleHelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+	
+}
+
