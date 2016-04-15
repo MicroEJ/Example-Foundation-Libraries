7a972d6a484af1d79d8e49dde63a513baea72a06
privron <privron@is2t-intern.com>
2016-04-15 10:19:34 +0200

    Add ej.examples.foundation.microui.out

---
+++ ej.examples.foundation.microui.out/README.md
@@ -0,0 +1,39 @@
+This example shows how to do the redirection of the standard SystemOut to the display
+
+#Run on the Simulator#
+1. Right Click on ExampleSysOutRedirection
+2. Select Run as -> Run Configuration 
+3. Click on new launch configuration
+4. Go to Execution tab
+5. Select your jpf 
+6. Go to Configuration tab
+7. Go to Libraries -> EDC
+8. Check Use a custom Java output stream
+9. Put the class ej.examples.foundation.microui.out.OutputStreamRedirection
+10. Press Apply
+11. Press Run
+
+#Run on the board#
+1. Right Click on ExampleSysOutRedirection
+2. Select Run as -> Run Configuration 
+3. Click on new launch configuration
+4. Select Execute on EmbJPF
+5. Select Build & Deploy
+6. Go to Execution tab
+7. Select your jpf 
+6. Go to Configuration tab
+7. Go to Libraries -> EDC
+8. Check Use a custom Java output stream
+9. Put the class ej.examples.foundation.microui.out.OutputStreamRedirection
+15. Press Apply
+16. Press Run
+17. Select Run -> Run Configuration
+18. Select MicroEJ Tool
+19. Click on new launch configuration
+20. Select your jpf 
+21. Select program with St link
+22. Go to Configuration tab
+23. Put the path to the generated .out file
+24. Press Apply
+25. Press Run
+
---
+++ ej.examples.foundation.microui.out/src/main/java/ej/examples/foundation/microui/out/DisplayableOutputStream.java
@@ -0,0 +1,51 @@
+/**
+ * Java
+ *
+ * Copyright 2009-2014 IS2T. All rights reserved.
+ * For demonstration purpose only.
+ * IS2T PROPRIETARY. Use is subject to license terms.
+ */
+package ej.examples.foundation.microui.out;
+
+import ej.microui.display.Display;
+import ej.microui.display.Displayable;
+import ej.microui.display.GraphicsContext;
+import ej.microui.util.EventHandler;
+
+/**
+ * Used to render all {@link Line}s in the {@link Display}.
+ */
+public class DisplayableOutputStream extends Displayable{
+
+	private final OutputStreamRedirection outputStreamRedirection;
+
+	public DisplayableOutputStream(Display display, OutputStreamRedirection outputStreamRedirection) {
+		super(display);
+		this.outputStreamRedirection = outputStreamRedirection;
+	}
+
+	@Override
+	public void paint(GraphicsContext g) {
+		Line[] lines = outputStreamRedirection.getLines();
+		int lenght = outputStreamRedirection.getLength();
+
+		// erase background
+		g.setColor(OutputStreamRedirection.COLOR_BACK);
+		g.fillRect(0, 0, g.getClipWidth(), g.getClipHeight());
+
+		// draw lines
+		int y = 0;
+		g.setColor(OutputStreamRedirection.COLOR_FONT);
+		for(int i = -1; ++i<lenght;) {
+			Line line = lines[i];
+			g.drawChars(line.getChars(), 0, line.getLength(), OutputStreamRedirection.TEXT_OFFSET, y, GraphicsContext.TOP | GraphicsContext.LEFT);
+			y += line.getFont().getHeight();
+		}
+	}
+
+	@Override
+	public EventHandler getController() {
+		// Nothing todo
+		return null;
+	}
+}
