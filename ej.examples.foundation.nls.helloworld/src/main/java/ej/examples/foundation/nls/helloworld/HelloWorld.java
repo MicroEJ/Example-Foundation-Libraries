2012a41f58e4c1276bf83c6abd7c332f2f73c3e5
privron <privron@is2t-intern.com>
2016-04-15 11:16:02 +0200

    Add ej.examples.foundation.nls.helloworld

---
+++ ej.examples.foundation.nls.helloworld/src/main/java/ej/examples/foundation/nls/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Java
+ * 
+ * Copyright 2010-2012 IS2T. All rights reserved.
+ * For demonstration purpose only.
+ * IS2T PROPRIETARY. Use is subject to license terms.
+ */
+package ej.examples.foundation.nls.helloworld;
+
+/**
+ * Interface that list the existing messages for the header "HelloWorld".
+ */
+public interface HelloWorld {
+
+	public static final int HELLO_WORLD		= 1;
+
+}
