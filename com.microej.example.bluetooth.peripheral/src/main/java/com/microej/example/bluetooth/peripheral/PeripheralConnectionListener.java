d17de2ffda5a666d478b033910cbac53d8a6f403
Andy Poudret <andy.poudret@microej.com>
2020-04-24 18:07:52 +0200

    J0059E-17 Fix copyrights

--- com.microej.example.bluetooth.peripheral/src/main/java/com/microej/example/bluetooth/peripheral/PeripheralConnectionListener.java
+++ com.microej.example.bluetooth.peripheral/src/main/java/com/microej/example/bluetooth/peripheral/PeripheralConnectionListener.java
@@ -1,15 +1,16 @@
 /*
  * Java
  *
- * Copyright 2018-2020 IS2T. All rights reserved.
- * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright 2018-2020 MicroEJ Corp. All rights reserved.
+ * This library is provided in source code for use, modification and test, subject to license terms.
+ * Any modification of the source code will break MicroEJ Corp. warranties on the whole library.
  */
 package com.microej.example.bluetooth.peripheral;
 
 import ej.bluetooth.BluetoothAdapter;
 import ej.bluetooth.BluetoothCharacteristic;
-import ej.bluetooth.BluetoothDescriptor;
 import ej.bluetooth.BluetoothConnection;
+import ej.bluetooth.BluetoothDescriptor;
 import ej.bluetooth.BluetoothObjectNotFoundException;
 import ej.bluetooth.BluetoothService;
 import ej.bluetooth.listeners.impl.DefaultConnectionListener;
